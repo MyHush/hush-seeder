c68604529322d4f607e2c9d135e312b003e4d8e5
FireMartZ <martindube@martindube.com>
2019-10-24 19:03:51 +0000

    Swap magic bytes.

--- coin.h
+++ coin.h
@@ -31,7 +31,7 @@ static const int testnet_port = 28030;
 //static unsigned char pchMessageStart[4] = { 0x24, 0xe9, 0x27, 0x64 };
 
 // HUSH3: magic.d394d36e
-static unsigned char pchMessageStart[4] = { 0xd3, 0x94, 0xd3, 0x6e };
+static unsigned char pchMessageStart[4] = { 0x6e, 0xd3, 0x94, 0xd3 };
 static unsigned char pchMessageStart_testnet[4] = { 0xfa, 0x1a, 0xf9, 0xbf };
 
 
